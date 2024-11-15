02d3878047d738d9714deb325f2ba11c7049ee9e
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-15 13:24:25 +0530

    Update layout.tsx

--- src/app/checkout/layout.tsx
+++ src/app/checkout/layout.tsx
@@ -2,8 +2,8 @@ import { type ReactNode } from "react";
 import { AuthProvider } from "@/ui/components/AuthProvider";
 
 export const metadata = {
-	title: "Saleor Storefront example",
-	description: "Starter pack for building performant e-commerce experiences with Saleor.",
+	title: "Seccretgreen.com.au",
+	description: "Home Improvement, DIY and Garden Online Store - Australia.",
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
