4270be3c0545ee8dd812a6253b9d6c248a1e3268
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-15 14:34:53 +0530

    Update Logo.tsx

--- src/ui/components/Logo.tsx
+++ src/ui/components/Logo.tsx
@@ -3,7 +3,7 @@
 import { usePathname } from "next/navigation";
 import { LinkWithChannel } from "../atoms/LinkWithChannel";
 
-const companyName = "Secertgreen";
+const companyName = "Secretgreen";
 
 export const Logo = () => {
 	const pathname = usePathname();
