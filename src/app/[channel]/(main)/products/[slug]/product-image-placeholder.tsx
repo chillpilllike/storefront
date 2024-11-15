5b19aef7c2e29b66e1b7eee903f7df5e414cde5d
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-15 19:04:25 +0530

    Update product-image-placeholder.tsx

--- src/app/[channel]/(main)/products/[slug]/product-image-placeholder.tsx
+++ src/app/[channel]/(main)/products/[slug]/product-image-placeholder.tsx
@@ -1,7 +1,7 @@
 import { type ComponentProps } from "react";
 
-import { ReactComponent as ProductPlaceholder } from "@/assets/product_placeholder.svg";
-import { cn } from "@/lib/utils";
+import { ReactComponent as ProductPlaceholder } from "./product_placeholder.svg";
+import { cn } from "./utils";
 
 type ProductImagePlaceholderProps = ComponentProps<"svg">;
 
