5a0ab93f3d8e8dc0cd4108dd1a4f160eddf3b5fe
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-15 13:05:30 +0530

    Update Footer.tsx

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -84,13 +84,7 @@ export async function Footer({ channel }: { channel: string }) {
 				<div className="flex flex-col justify-between border-t border-neutral-200 py-10 sm:flex-row">
 					<p className="text-sm text-neutral-500">Copyright &copy; {currentYear} secretgreen.com.au.</p>
 					<p className="flex gap-1 text-sm text-neutral-500">
-						Powered by{" "}
-						<Link target={"_blank"} href={"https://saleor.io/"}>
-							Saleor
-						</Link>{" "}
-						<Link href={"https://github.com/saleor/saleor"} target={"_blank"} className={"opacity-30"}>
-							<Image alt="Saleor github repository" height={20} width={20} src={"/github-mark.svg"} />
-						</Link>
+						Secure & Encrypted Payments{" "}
 					</p>
 				</div>
 			</div>
