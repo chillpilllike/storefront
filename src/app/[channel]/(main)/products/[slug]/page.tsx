30868331e2c58d41872278f56351771064b07891
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-16 19:35:22 +0530

    Update page.tsx

--- src/app/[channel]/(main)/products/[slug]/page.tsx
+++ src/app/[channel]/(main)/products/[slug]/page.tsx
@@ -6,6 +6,7 @@ import xss from "xss";
 import { invariant } from "ts-invariant";
 import { type WithContext, type Product } from "schema-dts";
 import { AddButton } from "./AddButton";
+import Carousel from "./Carousel";
 import { VariantSelector } from "@/ui/components/VariantSelector";
 // import { ProductImageWrapper } from "@/ui/atoms/ProductImageWrapper";
 import { executeGraphQL } from "@/lib/graphql";
@@ -13,7 +14,6 @@ import { formatMoney, formatMoneyRange } from "@/lib/utils";
 import { CheckoutAddLineDocument, ProductDetailsDocument, ProductListDocument } from "@/gql/graphql";
 import * as Checkout from "@/lib/checkout";
 import { AvailabilityMessage } from "@/ui/components/AvailabilityMessage";
-import Carousel from "./Carousel";
 
 export async function generateMetadata(
 	{
