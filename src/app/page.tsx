8ce4626ff324b93b74e286a1dd94da7dcde2e125
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-17 22:56:21 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from "next/navigation";
 
 export default function EmptyPage() {
-	redirect("/default-channel");
+	// redirect("/default-channel");
+	redirect("/channel-pln");
 }
