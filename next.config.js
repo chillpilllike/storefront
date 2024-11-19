0aa070ad4f2058b405520de1ff3a92c418d7a82d
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-20 00:25:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,21 +3,14 @@ const config = {
 	images: {
 		remotePatterns: [
 			{
-				protocol: "https",
-				hostname: "secretgreen9.s3.ap-southeast-2.amazonaws.com", // S3 bucket
-				pathname: "/**", // Allow all paths
-			},
-			{
-				protocol: "https",
-				hostname: "secretgreen-secretgreen-backend.g5edov.easypanel.host", // Backend hostname
-				pathname: "/**", // Allow all paths
+				hostname: "*",
 			},
 		],
 	},
 	experimental: {
 		typedRoutes: false,
 	},
-	// Used in the Dockerfile
+	// used in the Dockerfile
 	output:
 		process.env.NEXT_OUTPUT === "standalone"
 			? "standalone"
