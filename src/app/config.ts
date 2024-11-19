04a85da0cb1e11cb9cf96251cc86f517c7700e72
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-19 17:46:36 +0530

    Update config.ts

--- src/app/config.ts
+++ src/app/config.ts
@@ -2,7 +2,7 @@ import { createSaleorAuthClient } from "@saleor/auth-sdk";
 import { getNextServerCookiesStorage } from "@saleor/auth-sdk/next/server";
 import { invariant } from "ts-invariant";
 
-export const ProductsPerPage = 12;
+export const ProductsPerPage = 24;
 
 const saleorApiUrl = process.env.NEXT_PUBLIC_SALEOR_API_URL;
 invariant(saleorApiUrl, "Missing NEXT_PUBLIC_SALEOR_API_URL env variable");
