1e671f4c6fb97704c4ac1ad21c6aab30b841d791
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-19 19:29:40 +0530

    Update Footer.tsx

--- src/ui/components/Footer.tsx
+++ src/ui/components/Footer.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+// import Link from "next/link";
 import Image from "next/image";
 import { LinkWithChannel } from "../atoms/LinkWithChannel";
 import { ChannelSelect } from "./ChannelSelect";
